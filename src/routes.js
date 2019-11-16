f0ea0f2be1ce3ec2de8faa3dfca474f00fad3a00
Robson Magno Ramos das Neves <robsonmagno@MacBook-Pro-de-Robson.local>
2019-11-16 17:56:55 -0400

    Adiciona loader de models.

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Testando.' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego Fernandes',
+    email: 'diego@rocketseat.com.br',
+    password_hash: '123456',
+  });
+
+  res.json(user);
+});
 
 export default routes;
