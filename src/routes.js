e481e06edaf3e21520db80961bce5b11a0568936
Robson Magno Ramos das Neves <robsonmagno@MacBook-Pro-de-Robson.local>
2019-11-17 09:22:41 -0400

    Adiciona Middleware de autenticação.

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,17 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
+import authMeddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
+
 routes.post('/users', UserController.store);
+
+// Outra forma de utilizar os middlewares
+// routes.use(authMeddleware);
+
+routes.put('/users', authMeddleware, UserController.update);
 
 export default routes;
